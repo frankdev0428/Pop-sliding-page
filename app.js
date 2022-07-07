1f8d57fce6f2e1ea50a4ef4fff4b6fdd4cdaae6f
Hoang Le <frankdev0428@gmail.com>
2022-07-06 18:58:06 -0700

    initialine

---
+++ app.js
@@ -0,0 +1,25 @@
+const open_btn = document.querySelector('.open-btn');
+const close_btn = document.querySelector('.close-btn');
+const popup = document.querySelector('.popup');
+const main_popup = document.querySelector('.main-popup');
+
+open_btn.addEventListener('click', () => {
+	 popup.style.display = 'flex';
+	main_popup.style.cssText = 'animation:slide-in .5s ease; animation-fill-mode: forwards;';
+});
+
+close_btn.addEventListener('click', () => {
+	main_popup.style.cssText = 'animation:slide-out .5s ease; animation-fill-mode: forwards;';
+	setTimeout(() => {
+		popup.style.display = 'none';
+	}, 500);
+});
+
+window.addEventListener('click', (e) => {
+	if (e.target == document.querySelector('.popup-overlay')) {
+		main_popup.style.cssText = 'animation:slide-out .5s ease; animation-fill-mode: forwards;';
+		setTimeout(() => {
+			popup.style.display = 'none';
+		}, 500);
+	}
+});
